40dd7485ecccfe3cc2e84101e883c906087e3b3e
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-17 15:24:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniRailsDemo::Application.config.secret_token = 'c9b08aa7cd45e6a7dd9701fea24059d5d75461c1030470baf531debcf9e63a8dbffb46312602556322dd6a4dcd2b8084ae7d0d9984acc0d48fded94f2b85cc7c'
