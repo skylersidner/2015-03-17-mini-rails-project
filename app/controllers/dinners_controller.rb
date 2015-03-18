9f35ca19508235faebadf3a1ba05f41958219372
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-17 23:26:04 -0500

    full CRUD functionality; needs validation and redirecting;

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,6 +1,26 @@
 class DinnersController < ApplicationController
   def save
     @dinner = Dinner.create(main_source: params[:main_source], sauce: params[:sauce], starch: params[:starch], veggie: params[:veggie])
+    #redirect_to() #can't figure this out
+  end
+  
+  def show
+    @dinner = Dinner.find_by_id(params[:id])
+  end
+  
+  def edit
+    @dinner = Dinner.find_by_id(params[:id])
+  end
+  
+  def update
+    @dinner = Dinner.find_by_id(params[:id])
+    @dinner.update_attributes(main_source: params[:main_source], sauce: params[:sauce], starch: params[:starch], veggie: params[:veggie])
+    #redirect_to() #can't figure this out
+  end
+  
+  def destroy
+    @dinner = Dinner.find_by_id(params[:id])
+    @dinner.destroy
   end
     
   def list_all
