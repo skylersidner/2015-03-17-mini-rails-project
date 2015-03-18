fc2d6d19d4c4f0a6e94a610ce6c1dec04ca7ac4f
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-17 21:53:41 -0500

    changed homepage; create/save working; need to add id column to table;

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,4 +1,8 @@
 class DinnersController < ApplicationController
+  def save
+    @dinner = Dinner.create(main_source: params[:main_source], sauce: params[:sauce], starch: params[:starch], veggie: params[:veggie])
+  end
+    
   def list_all
     @list = Dinner.all
   end
