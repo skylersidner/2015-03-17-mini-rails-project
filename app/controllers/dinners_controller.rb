fe435696710d47f571797c687e514fa463940dc6
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-18 09:12:27 -0500

    Added redirects, changed routes to use symbols instead of splats

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,7 +1,7 @@
 class DinnersController < ApplicationController
   def save
     @dinner = Dinner.create(main_source: params[:main_source], sauce: params[:sauce], starch: params[:starch], veggie: params[:veggie])
-    #redirect_to() #can't figure this out
+    redirect_to "/dinners/#{@dinner.id}/show"
   end
   
   def show
@@ -15,7 +15,7 @@ class DinnersController < ApplicationController
   def update
     @dinner = Dinner.find_by_id(params[:id])
     @dinner.update_attributes(main_source: params[:main_source], sauce: params[:sauce], starch: params[:starch], veggie: params[:veggie])
-    #redirect_to() #can't figure this out
+    redirect_to "/dinners/#{@dinner.id}/show"
   end
   
   def destroy
